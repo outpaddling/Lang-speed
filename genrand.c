9899ea847377dec7cddb130bc91c07dcda0b134b
Jason Bacon <bacon@moray.acadix.biz>
2021-11-11 18:58:12 -0600

    clang-check: Compare subscript and pointer performance

--- genrand.c
+++ genrand.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ *  Generate N pseudo-random numbers that fit in a C int for benchmarking
+ *  selection sort.  Randomness is not important since selection sort is
+ *  unaffected by initial sequence, unlike quicksort.
+ */
+
 int     main(int argc,char *argv[])
 
 {
     int     c,
 	    count;
     
-    count = atoi(argv[1]);
+    count = atol(argv[1]);
     printf("%d\n",count);
     for (c=0; c<count; ++c)
-	printf("%d\n",rand());
+	printf("%d\n",(int)random());
     return 0;
 }
 
